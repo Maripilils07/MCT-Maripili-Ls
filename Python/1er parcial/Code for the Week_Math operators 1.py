2eb0f3fa144cf0774c4decfa436dbd6c3f874097
Maripilils07 <104381820+Maripilils07@users.noreply.github.com>
2022-05-30 22:04:21 -0500

    Add files via upload

---
+++ Python/1er parcial/Code for the Week_Math operators 1.py
@@ -0,0 +1,13 @@
+greeting= "Hi,how are you?"
+print(greeting)
+print("enter your answer:")
+greeting=input()
+name= "What is your name?"
+print(name)
+print('enter your answer:')
+name=input()
+age=('Who old are you?')
+print('age')
+
+
+
---
+++ Python/1er parcial/Presentation using constants.py
@@ -0,0 +1,40 @@
+print('Code for the week 1')
+print('Grades:')
+math1=10
+math2=9
+math3=10
+suma1=math1+math2+math3
+divi1=suma1/3
+print('The math average is:',divi1)
+
+LA1=7
+LA2=9
+LA3=8
+suma2=LA1+LA2+LA3
+divi2=suma2/3
+print('The LA average is:',divi2)
+
+MCT1=4
+MCT2=5
+MCT3=8
+suma3=MCT1+MCT2+MCT3
+divi3=suma3/3
+print('The MCT average is:',divi3)
+
+CyE1=7
+CyE2=10
+CyE3=10
+suma4=CyE1+CyE2+CyE3
+divi4=suma4/3
+print('The average Civics and Ethics is:',divi4)
+
+science1=9
+science2=10
+science3=10
+suma5=science1+science2+science3
+divi5=suma5/3
+print('The average of Science is:',divi5)
+
+suma6=divi1+divi2+divi3+divi4+divi5
+divi6=suma6/5
+print('The general average is:',divi6)
